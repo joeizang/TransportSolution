ee90291963adcb684c5d8dd44e8ad3037464e48e
Joseph Izang <josephizang@gmail.com>
2024-08-17 03:33:13 +0100

    set things up add clean architecture based layers.

---
+++ TransportSolution.Infrastructure/DepenedencyInjection.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TransportSolution.Infrastructure;
+
+public static class DepenedencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ TransportSolution.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TransportSolution.WebUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
